a4aaea52defecf10835af9b9975bc174e08ba982
Zhu Xiaochen <42903201+zhXchD@users.noreply.github.com>
2020-10-19 23:15:12 +0800

    Revert "Enhence testing coverage"

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -200,8 +200,7 @@ public class ModelManager implements Model {
         ModelManager other = (ModelManager) obj;
         return addressBook.equals(other.addressBook)
             && userPrefs.equals(other.userPrefs)
-            && filteredPersons.equals(other.filteredPersons)
-            && filteredEntries.equals(other.filteredEntries);
+            && filteredPersons.equals(other.filteredPersons);
     }
 
 }
